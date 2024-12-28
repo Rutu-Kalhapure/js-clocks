e89da8fa6713fafdc9fb5f3ae247b7f2e6a2f47b
Rutu Kalhapure <rutukalhapure2610@gmail.com>
2024-12-28 16:24:37 +0530

    Pushing all files

---
+++ analog-Clocks/style2/script.js
@@ -0,0 +1,23 @@
+// Get references to DOM elements
+const hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second");
+
+const updateTime = () => {
+  // Get current time and calculate degrees for clock hands
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+  // Rotate the clock hands to the appropriate degree based on the current time
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+// Call updateTime to set clock hands every second
+setInterval(updateTime, 1000);
+
+// Call updateTime function on page load
+updateTime();
